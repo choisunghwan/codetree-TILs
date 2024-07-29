40e5c0cdc09954df1b32d5b834226e200cef2cad
Sunghwan <33335762+choisunghwan@users.noreply.github.com>
2024-07-30 08:01:50 +0900

    Title: 실수와 문자 받아 출력하기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240730/실수와 문자 받아 출력하기/print-number--and-letter.py
+++ 240730/실수와 문자 받아 출력하기/print-number--and-letter.py
@@ -4,5 +4,8 @@ b = float(input())
 
 
 print(c)
-print(round(a,2))
-print(round(b,2))
+print(f"{a:.2f}") # 소수점 둘째 자리까지 반올림하여 출력
+print(f"{b:.2f}") # 소수점 둘째 자리까지 반올림하여 출력
+
+# print()와 f-string을 사용하여 소수점 둘째 자리까지 반올림된 값을 출력합니다. 
+# f-string을 사용하면 f"{a:.2f}"와 같이 소수점 둘째 자리까지 표시할 수 있습니다
