353bdef9da606d98e38e4fe31fe9868e50668a05
Sunghwan <33335762+choisunghwan@users.noreply.github.com>
2024-08-16 15:20:27 +0900

    Title: 자연수면 출력, Time: 161ms, Memory: 24MB, Status: Passed - CodeTree

--- 240816/자연수면 출력/output-only-natural-number.py
+++ 240816/자연수면 출력/output-only-natural-number.py
@@ -1,10 +1,6 @@
 a, b = map(int, input().split())
-temp = []
 
-for _ in range(b):
-    if a <= 0:
-        print(0)
-    else:
-        temp.append(a)
-
-print("".join(map(str,temp)))
+if a <= 0:
+    print(0)
+else:
+    print(str(a)*b)
